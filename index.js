09093b3c926ee9c34d3636839313c21b0fe3a9b6
Samuel Meuli <me@samuelmeuli.com>
2019-02-04 21:19:44 +0100

    Add config

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+	extends: [
+		// Reasonable config for CSS and SCSS
+		"stylelint-config-sass-guidelines",
+		// Config for enforcing property order
+		"stylelint-config-rational-order",
+	],
+
+	rules: {
+		// Rule overrides for stylelint-config-sass-guidelines
+		"color-hex-length": "long",
+		indentation: "tab",
+		"max-nesting-depth": null,
+		"order/properties-alphabetical-order": null,
+		"selector-no-qualifying-type": null,
+
+		// Additional rules
+		"declaration-no-important": true,
+		"font-family-name-quotes": "always-unless-keyword",
+		"font-weight-notation": "numeric",
+		"function-url-no-scheme-relative": true,
+		"selector-attribute-quotes": "always",
+		"string-quotes": "double",
+	},
+};
